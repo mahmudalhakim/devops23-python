6901af602fcb7756bb85ea5e011d1e87b4897db4
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-09-01 12:43:13 +0200

    While-satsen

---
+++ Lektion03/while.py
@@ -0,0 +1,22 @@
+counter = 2
+while counter <= 10:
+    #print(counter)
+    counter = counter + 2
+
+while True:
+  print("This program is running")
+  goAgain = input("Go again (y/n)? ")
+  if goAgain == "n":
+    break
+  
+print("Aww, I was having a good time 😭")
+
+
+while True:
+    color = input("Enter a color: ")
+    if color == "red":
+        break
+    else:
+        print("Cool color!")
+
+print("I don't like red.")
