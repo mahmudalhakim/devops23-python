43b6d97afc3d6b09c4b9f80b5459b85e80d90854
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-09-04 10:00:30 +0200

    Try Except Demo

---
+++ Lektion04/try-except-demo.py
@@ -0,0 +1,8 @@
+# Exempel på try-except i Python
+try:
+    indata = input("Ange ett heltal:")
+    tal = int(indata)
+    kvadrat = tal * tal
+    print(tal, "i kvadrat är", kvadrat)
+except:
+    print(indata , "är inte ett heltal")
