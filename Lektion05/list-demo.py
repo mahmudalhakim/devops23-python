6b217512db72a434ceab81f4cd87d1e3ce613119
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-09-06 11:32:58 +0200

    Iterationer

--- Lektion05/list-demo.py
+++ Lektion05/list-demo.py
@@ -6,4 +6,17 @@ while i < len(deltagare):
 
 deltagare = ["Lina", "Gunilla", "Erik", "Carl"]
 for namn in deltagare:
-  print(f"Välkommen {namn}")
+    print(f"Välkommen {namn}")
+
+# Övning
+# Skriv ut alla tal från 0 till 9 utan att använda while
+myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in myList:
+    print(i)
+
+for i in range(10):
+    print(i)
+
+till_9 = list(range(10))
+for tal in till_9:
+    print(tal)
