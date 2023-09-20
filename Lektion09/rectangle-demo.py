ecff1c2a7a9e6eb720f81b1efa0819422a2ad68b
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-09-20 14:06:40 +0200

    OOP

--- Lektion09/rectangle-demo.py
+++ Lektion09/rectangle-demo.py
@@ -4,23 +4,23 @@ from random import randint
 
 class Rectangle:
 
-    def __init__(self, length, width):
+    def __init__(self, height, width):
         # Privata attribut
-        self.__length = length
+        self.__height = height
         self.__width = width
 
     # Getters & Setters
-    def get_length(self):
-        return self.__length
+    def get_height(self):
+        return self.__height
 
     def get_width(self):
         return self.__width
 
-    def set_length(self, length):
-        if length > 0:
-            self.__length = length
+    def set_height(self, height):
+        if height > 0:
+            self.__height = height
         else:
-            print("Felaktig längd!")
+            print("Felaktig höjd!")
 
     def set_width(self, width):
         if width > 0:
@@ -30,24 +30,24 @@ class Rectangle:
 
     # Metoder
     def area(self):
-        return self.get_length() * self.get_width()
+        return self.get_height() * self.get_width()
 
     def omkrets(self):
-        return (self.get_length() + self.get_width()) * 2
+        return (self.get_height() + self.get_width()) * 2
 
 
 # Skapa objekt av klassen Rectangle
 r1 = Rectangle(1, 2)
-print(r1.get_length())
+print(r1.get_height())
 print(r1.get_width())
 
 # Ändra attribut
 r1.set_width(-5)
-r1.set_length(10)
+r1.set_height(10)
 
 print("----- r1 -----")
 print("Bredd:", r1.get_width())
-print("Längd", r1.get_length())
+print("Längd", r1.get_height())
 
 print("Area:", r1.area())
 print("Omkrets:", r1.omkrets())
@@ -55,7 +55,7 @@ print("Omkrets:", r1.omkrets())
 print("----- r2 -----")
 r2 = Rectangle(2, 3)
 r2.set_width(10)
-r2.set_length(10)
+r2.set_height(10)
 print("Area:", r2.area())
 print("Omkrets:", r2.omkrets())
 print("----------------------")
